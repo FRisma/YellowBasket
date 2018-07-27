9e209e448cff9b4bb3348888bf4e147bc721ef2e
Franco Risma <franco.emilio.risma@fisglobal.com>
2018-07-27 18:09:18 -0300

    Adding assets

---
+++ Support/Utils.swift
@@ -0,0 +1,70 @@
+//
+//  Utils.swift
+//  YellowBasket
+//
+//  Created by Franco Risma on 27/07/2018.
+//  Copyright © 2018 FRisma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+/**
+ * Language
+ */
+
+let kITLang = "lang"
+
+/**
+ * Style
+ */
+
+let kLaunchScreenTopColor = UIColor(0xf8ff47)
+let kLaunchScreenBottomColor = UIColor(0xfaff89)
+
+/**
+ * @brief Add the ability for UIColor to be initialized with hex values
+ *
+ * @discussion Currently there is no implementation in the iOS SDK for initializing a color from
+ * an hexadecimal value
+ */
+extension UIColor {
+    convenience init(_ hex: UInt) {
+        self.init(
+            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(hex & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
+
+/**
+ * @brief Shake animation for every view
+ *
+ * @discussion Just call yourview.shake() and you'll get the animation
+ */
+extension UIView {
+    func shake(view: UIView) {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.y"
+        animation.values = [0, 10, -10, 10, -5, 5, -5, 0 ]
+        animation.keyTimes = [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
+        animation.duration = 1
+        animation.isAdditive = true
+        
+        view.layer.add(animation, forKey: "shake")
+    }
+}
+
+
+class GradientColors {
+    var layer:CAGradientLayer!
+    
+    init(withTopColor topC: UIColor, bottomColor bottomC: UIColor) {
+        layer = CAGradientLayer()
+        layer.colors = [topC.cgColor, bottomC.cgColor]
+        layer.locations = [0.0, 1.0]
+    }
+}
--- YellowBasket/Launch/LaunchViewController.swift
+++ YellowBasket/Launch/LaunchViewController.swift
@@ -7,29 +7,34 @@
 //
 
 import UIKit
+import SnapKit
 
 class LaunchViewController: UIViewController {
 
+    private var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        imageView = UIImageView(image: UIImage(named: "launchScreen"))
+        view.addSubview(imageView)
+        
+        imageView.snp.makeConstraints { (make) in
+            make.center.equalTo(view)
+        }
+        
+        view.backgroundColor = .clear
+        let backgroundLayer = GradientColors(withTopColor: kLaunchScreenTopColor, bottomColor: kLaunchScreenBottomColor).layer
+        backgroundLayer?.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer!, at: 0)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
-    */
+    
+    override func viewDidAppear(_ animated: Bool) {
+        view.shake(view: imageView)
+    }
 
 }
